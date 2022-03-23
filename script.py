d801f40f976fce3063faad5bba7dbef37ec5d58e
Andreas.Goumas <andreas.goumas@axpo.group>
2022-03-23 14:41:43 +0100

    created authentication function in order to retrieve token

--- script.py
+++ script.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import requests
 import json
+
+
+def authenticate(usr, psw):
+    url = "https://ems.enerim.com/ExternalData/Users/Authenticate"
+
+    payload = json.dumps({
+        "username": usr,
+        "password": psw
+    })
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()['token']
